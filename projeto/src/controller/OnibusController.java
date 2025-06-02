551236e952e0837194f0d63b3ac6a9376410845e
Enzo Goulart <enzobgoulart@gmail.com>
2025-06-02 19:17:22 -0300

    onibus controller feito

--- projeto/src/controller/OnibusController.java
+++ projeto/src/controller/OnibusController.java
@@ -1,5 +1,64 @@
 package controller;
 
+import model.Onibus;
+import util.ArquivoTXT;
+import util.ArquivoPersistente;
+
+import java.util.*;
+
 public class OnibusController {
+    private List<Onibus> listaOnibus = new ArrayList<>();
+    private final ArquivoPersistente<Onibus> persistencia = new ArquivoTXT<>();
+    private final String caminhoArquivo = "dados/onibus.txt";
+
+    public OnibusController() {
+        carregar();
+    }
+
+    public void adicionarOnibus(Onibus onibus) {
+        listaOnibus.add(onibus);
+        salvar();
+    }
+
+    public List<Onibus> listarOnibus() {
+        return listaOnibus;
+    }
+
+    public void atualizarOnibus(int numeroId, String novoModelo, int novaCapacidade) {
+    Onibus o = buscarPorIdentificador(numeroId);
+        if (o != null) {
+            o.setModelo(novoModelo);
+            o.setCapacidade(novaCapacidade);
+            salvar();
+        }
+    }
     
+    public boolean removerPorIdentificador(int numeroId) {
+        Onibus o = buscarPorIdentificador(numeroId);
+        if (o != null) {
+            listaOnibus.remove(o);
+            salvar();
+            return true;
+        }
+        return false;
+    }
+
+
+    public Onibus buscarPorIdentificador(int numeroId) {
+        for (Onibus o : listaOnibus) {
+            if (o.getNumeroIdentificacao() == numeroId) {
+                return o;
+            }
+        }
+        return null;
+    }
+
+
+    private void salvar() {
+        persistencia.salvar(listaOnibus, caminhoArquivo);
+    }
+
+    private void carregar() {
+        listaOnibus = persistencia.carregar(caminhoArquivo, Onibus::fromString);
+    }
 }
--- projeto/src/model/Onibus.java
+++ projeto/src/model/Onibus.java
@@ -4,10 +4,12 @@ public class Onibus {
     
     private int numeroIdentificacao;
     private String modelo;
+    private int capacidade;
 
-    public Onibus(int numeroIdentificacao, String modelo) {
+    public Onibus(int numeroIdentificacao, String modelo, int capacidade) {
         this.numeroIdentificacao = numeroIdentificacao;
         this.modelo = modelo;
+        this.capacidade = capacidade;
     }
 
     // Getters e Setters
@@ -18,12 +20,31 @@ public class Onibus {
     public String getModelo() {
         return modelo;
     }
+    public int getCapacidade() {
+        return capacidade;
+    }
 
     public void setNumeroIdentificacao(int numeroIdentificacao) {
         this.numeroIdentificacao = numeroIdentificacao;
     }
     public void setModelo(String modelo) {
         this.modelo = modelo;
+    }
+    public void setCapacidade(int capacidade) {
+        this.capacidade = capacidade;
+    }
+
+    public static Onibus fromString(String linha) {
+    String[] partes = linha.split(";");
+    int numeroIdentificacao = Integer.parseInt(partes[0]);
+    String modelo = partes[1];
+    int capacidade = Integer.parseInt(partes[2]);
+    return new Onibus(numeroIdentificacao, modelo, capacidade);
+    }
+
+    @Override
+    public String toString() {
+        return numeroIdentificacao + ";" + modelo + ";" + capacidade;
     }
 
 }
