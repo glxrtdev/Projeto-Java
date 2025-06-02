a3ba444d30eefc55a500e05a66ec8e6b69dac919
Enzo Goulart <enzobgoulart@gmail.com>
2025-06-02 18:41:48 -0300

    util

--- projeto/src/util/ArquivoPersistente.java
+++ projeto/src/util/ArquivoPersistente.java
@@ -1,5 +1,8 @@
 package util;
 
-public class ArquivoPersistente {
-    
+import java.util.List;
+
+public interface ArquivoPersistente<T> {
+    void salvar(List<T> lista, String caminho);
+    List<T> carregar(String caminho);
 }
--- projeto/src/util/ArquivoTXT.java
+++ projeto/src/util/ArquivoTXT.java
@@ -1,5 +1,26 @@
 package util;
 
-public class ArquivoTXT {
-    
+import java.io.*;
+import java.util.*;
+
+public class ArquivoTXT<T> implements ArquivoPersistente<T> {
+
+    @Override
+    public void salvar(List<T> lista, String caminho) {
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter(caminho))) {
+            for (T obj : lista) {
+                writer.write(obj.toString()); // Importante: o toString() deve retornar o formato gravável
+                writer.newLine();
+            }
+        } catch (IOException e) {
+            System.out.println("Erro ao salvar dados: " + e.getMessage());
+        }
+    }
+
+    @Override
+    public List<T> carregar(String caminho) {
+        // Esse método é genérico, mas não consegue reconstruir objetos sem saber o tipo.
+        // A leitura deve ser feita com lógica específica nos controllers.
+        return new ArrayList<>();
+    }
 }
