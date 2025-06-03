9c2fc6ccb67ccb2640379e026f28059dec46dc65
Enzo Goulart <enzobgoulart@gmail.com>
2025-06-03 16:39:08 -0300

    ViagemView adicionado

---
+++ projeto/src/view/ViagemView.java
@@ -0,0 +1,95 @@
+package view;
+
+import java.util.Scanner;
+import controller.ViagemController;
+import model.Viagem;
+
+public class ViagemView {
+    
+    private Scanner scanner = new Scanner(System.in);
+    ViagemController vc = new ViagemController();
+
+    public void menuViagem() {
+        System.out.println("Escolha uma opção: ");
+        System.out.println("[1] Adicionar viagem");
+        System.out.println("[2] Listar viagens");
+        System.out.println("[3] Buscar viagem");
+        System.out.println("[4] Editar viagem");
+        System.out.println("[5] Remover viagem");
+        int op = scanner.nextInt();
+        scanner.nextLine();
+
+        switch (op) {
+            case 1: adicionarViagem();                
+                break;
+            case 2: vc.listarViagens();
+                break;
+            case 3: buscarViagem();
+                break;
+            case 4: editarViagem();
+                break;
+            case 5: removerViagem();
+                break;
+            default:
+                break;
+        }
+    }
+
+
+    public void adicionarViagem() {
+        System.out.print("Dê um id para a viagem: ");
+        int id = scanner.nextInt();
+        scanner.nextLine();
+
+        System.out.print("Destino:");
+        String destino = scanner.nextLine();
+
+        System.out.print("Data (ex: 03/06/2025): ");
+        String data = scanner.nextLine();
+
+        System.out.print("Qual o id do ônibus que será utilizado?: ");
+        int idOnibus = scanner.nextInt();
+        scanner.nextLine();
+
+        Viagem novaViagem = new Viagem(id, destino, data, idOnibus);
+        vc.adicionarViagem(novaViagem);
+
+        System.out.println("Viagem adicionada com sucesso!");
+    }
+
+    public void buscarViagem() {
+        System.out.print("Informe o id da viagem: ");
+        int id = scanner.nextInt();
+        scanner.nextLine();
+
+        vc.buscarPorIdentificador(id);
+    }
+
+    public void editarViagem() {
+        System.out.println("Dê um novo ID da viagem: ");
+        int id = scanner.nextInt();
+        scanner.nextLine();
+
+        System.out.println("Novo destino: ");
+        String destino = scanner.nextLine();
+
+        System.out.println("Nova data: ");
+        String data = scanner.nextLine();
+
+        System.out.println("ID do ônibus: ");
+        int idOnibus = scanner.nextInt();
+        scanner.nextLine();
+
+        vc.atualizarViagem(id, destino, data, idOnibus);
+    }
+
+    public void removerViagem() {
+        System.out.println("Qual o ID da viagem?: ");
+        int id = scanner.nextInt();
+        scanner.nextLine();
+
+        vc.removerPorIdentificador(id);
+    }
+
+    
+}
