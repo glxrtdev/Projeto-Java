f3be39a0793943988be52464015330b531cf2f14
Enzo Goulart <enzobgoulart@gmail.com>
2025-06-03 17:54:08 -0300

    MotoristaController adicionado

---
+++ projeto/src/controller/MotoristaController.java
@@ -0,0 +1,63 @@
+package controller;
+
+import java.util.ArrayList;
+import java.util.List;
+import model.Motorista;
+import util.ArquivoPersistente;
+import util.ArquivoTXT;
+
+
+public class MotoristaController {
+    private List<Motorista> listaMotoristas = new ArrayList<>();
+    private final ArquivoPersistente<Motorista> persistencia = new ArquivoTXT<>();
+    private final String caminhoArquivo = "dados/motoristas.txt";
+
+    public MotoristaController() {
+        carregar();
+    }
+
+    public void adicionarMotorista(Motorista motorista) {
+        listaMotoristas.add(motorista);
+        salvar();
+    }
+
+    public List<Motorista> listarMotoristas() {
+        return listaMotoristas;
+    }
+
+    public Motorista buscarPorNome(String nomeMotorista) {
+        for (Motorista m : listaMotoristas) {
+            if (m.getNome() == nomeMotorista) {
+                return m;
+            }
+        }
+        return null;
+    }
+
+    public void atualizarMotorista(String nome, String cpf, double novoSalario, String cnh) {
+        Motorista m = buscarPorNome(nome);
+        if (m != null) {
+            m.setSalario(novoSalario);
+            salvar();
+        }
+    }
+
+    public boolean removerPorNome(String nome) {
+        Motorista m = buscarPorNome(nome);
+        if (m != null) {
+            listaMotoristas.remove(m);
+            salvar();
+            return true;
+        }
+        return false;
+    }
+
+    private void salvar() {
+        persistencia.salvar(listaMotoristas, caminhoArquivo);
+    }
+
+    private void carregar() {
+        listaMotoristas = persistencia.carregar(caminhoArquivo, Motorista::fromString);
+    }
+
+}
--- projeto/src/model/Motorista.java
+++ projeto/src/model/Motorista.java
@@ -31,4 +31,21 @@ public class Motorista extends Pessoa{
     public void setSalario(double salario) {
         this.salario = salario;
     }
+
+    public static Motorista fromString(String linha) {
+        String[] partes = linha.split(";");
+        String nome = partes[0];
+        String cpf = partes[1];
+        double salario = Double.parseDouble(partes[2]);
+        String cnh = partes[3];
+        
+        return new Motorista(nome, cpf, salario, cnh);
+    }
+
+    @Override
+    public String toString() {
+        return nome + ";" + cpf + ";" + salario + ";" + cnh;
+    }
+
+
 }
